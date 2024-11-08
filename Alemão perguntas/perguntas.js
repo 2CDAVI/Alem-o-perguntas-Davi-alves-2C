b214a15bf9db63d1f4dab17fd815a9cb26712171
2CDAVI <00001111374491sp@al.educacao.sp.gov.br>
2024-11-08 11:27:12 -0300

    Add files via upload

---
+++ Alemão perguntas/perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Anabolizantes',
+    'O que e anabolizantes?',
+    'São hormonios sinteticos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o pais com mais pessoas no mundo?',
+    'India '
+)
+
+criaCartao(
+    'Programação',
+    'O que é o canva?',
+    'plataforma de design grafico'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "voce esta bem" em Inglês?',
+    'are you well'
+)
